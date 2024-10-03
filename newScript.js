601b9d22ec6c0816d86419785c2ceae35cff9b16
OnyxDev87 <mileswilander@icloud.com>
2024-10-03 15:11:22 +0000

    Made it closer two working

--- newScript.js
+++ newScript.js
@@ -15,16 +15,39 @@ canvas.height = window.innerHeight;
 
 const gridSize = 7;
 const halfSize = Math.floor(gridSize / 2);
-const liveCells = new Map();
+const liveCells = new Set();
+
+function initializeGrid() {
+  const rows = Math.floor(canvas.height / gridSize);
+  const columns = Math.floor(canvas.width / gridSize);
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const x = i
+      const y = j
+      liveCells.add(`${x},${y}`);
+    }
+  }
+}
 
 function doTheThing() {
-    const rows = Math.floor(canvas.height / gridSize);
-    const columns = Math.floor(canvas.width / gridSize);
+  initializeGrid();
 
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-          let x = i * gridSize;
-          let y = j * gridSize;
-        }
-     }
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      const numAliveNeightbors;
+      if (liveCells.has(`${i},${j}`))
+      
+    }
+  }
+  
+  // try {
+  //   const cellState = liveCells.get("1,7");
+  //   alert(cellState || "Cell does not exist");
+  // } catch (error) {
+  //   console.error("An error occurred:", error);
+  //   alert("An error occurred while accessing the cell state.");
+  // }
 }
+
+doTheThing();
